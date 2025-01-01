b2b55ee7eb069367138ebf53b407c1f5f44913ea
RandQuran <siddiqmutarji.m@gmail.com>
2025-01-01 13:27:52 +0300

    h

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://profound-snickerdoodle-c52602.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), sitemap({
+    filter: (page) => page !== 'https://profound-snickerdoodle-c52602.netlify.app/internal',
+    xslURL: '/sitemap.xsl'
+  })]
 });
